27888af18653e628b2b76a6a91791bcca1e9b88a
Dave Gentilli <dgentilli@gmail.com>
2021-03-31 16:10:33 +0300

    Create theme file and begin defining theme colors.

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  color: {
+    BG_PRIMARY: '#e9ecefff',
+    BG_SECONDARY: '#343a40ff',
+    TEXT_PRIMARY: '#495057ff',
+    TEXT_SECONDARY: '#f8f9faff',
+    SKY_BLUE: '#4cc9f0ff',
+    LIGHT_GRAY: '#ced4daff',
+  },
+};
+
+export default theme;
