d15d6f4019c419452cfb389e4540625819060a18
Dave Gentilli <dgentilli@gmail.com>
2021-04-05 15:20:22 +0300

    Implement initial style in the MyStuff screen.

--- src/screens/MyStuff.js
+++ src/screens/MyStuff.js
@@ -1,33 +1,53 @@
 import React, {useContext} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import styled from 'styled-components';
 
+import theme from '../theme';
 import {Context} from '../../App';
+import {Container, Spacer, PrimaryHeading} from '../components/reusable/Main';
+
+const Row = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin: 0 auto;
+  height: 75px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${theme.color.LIGHT_GRAY};
+`;
+
+const CustomText = styled.Text`
+  color: ${props =>
+    props.theme === 'dark' ? theme.color.TEXT_LIGHT : theme.color.TEXT_DARK};
+  font-size: 16px;
+`;
 
 const MyStuff = () => {
   const navigation = useNavigation();
   const context = useContext(Context);
-  console.log('MyStuff context: ', context);
-  const {
-    isFirstLogin,
-    loggedInUser,
-    theme,
-    toggleTheme,
-    newsCategoryPreferences,
-  } = context;
-  console.log({toggleTheme});
+  //console.log('MyStuff context: ', context);
+  const {loggedInUser, theme, toggleTheme} = context;
   return (
-    <View style={{marginTop: 40}}>
-      <Text style={{textAlign: 'center', fontSize: 25}}>MyStuff Screen</Text>
-      <Text style={{textAlign: 'left', fontSize: 16}}>Theme Preferences</Text>
-      <TouchableOpacity onPress={toggleTheme}>
-        <Text>{theme}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('SetNewsPreferences')}>
-        <Text>Set News Preferences</Text>
-      </TouchableOpacity>
-    </View>
+    <Container theme={theme}>
+      <Spacer height={20} />
+      <PrimaryHeading theme={theme}>MyStuff Screen</PrimaryHeading>
+      <Spacer height={30} />
+      <Row>
+        <CustomText theme={theme}>Theme Preferences</CustomText>
+        <TouchableOpacity onPress={toggleTheme}>
+          <CustomText theme={theme}>{theme}</CustomText>
+        </TouchableOpacity>
+      </Row>
+      <Row>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('SetNewsPreferences')}>
+          <CustomText theme={theme}>Set News Preferences</CustomText>
+        </TouchableOpacity>
+      </Row>
+    </Container>
   );
 };
 
