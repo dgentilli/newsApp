70a648afe95695332af072ffc7a3e7cca5a85c06
Dave Gentilli <dgentilli@gmail.com>
2021-04-08 17:07:04 +0300

    Build AllNewsTopics screen with navigation.

--- src/screens/AllNewsTopics.js
+++ src/screens/AllNewsTopics.js
@@ -1,17 +1,75 @@
 import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
+import {FlatList} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 import {Context} from '../../App';
+import {
+  Container,
+  Button,
+  ButtonText,
+  Spacer,
+  PrimaryHeading,
+} from '../components/global/Main';
 
 const AllNewsTopics = () => {
+  const navigation = useNavigation();
   const context = useContext(Context);
+  const {theme} = context;
+  const allNewsCategories = [
+    'us',
+    'world',
+    'business',
+    'science',
+    'arts',
+    'automobiles',
+    'books',
+    'fashion',
+    'food',
+    'health',
+    'home',
+    'insider',
+    'magazine',
+    'movies',
+    'nyregion',
+    'obituaries',
+    'opinion',
+    'politics',
+    'realestate',
+    'sports',
+    'sundayreview',
+    'technology',
+    'theater',
+    't-magazine',
+    'travel',
+    'upshot',
+  ];
   //console.log('all news topics context: ', context);
   return (
-    <View style={{marginTop: 40}}>
-      <Text style={{textAlign: 'center', fontSize: 25}}>
-        AllNewsTopics Screen
-      </Text>
-    </View>
+    <Container theme={theme}>
+      <Spacer height={20} />
+      <PrimaryHeading theme={theme}>Browse by Section</PrimaryHeading>
+      <Spacer height={10} />
+      <FlatList
+        data={allNewsCategories}
+        keyExtractor={item => item}
+        renderItem={({item}) => {
+          return (
+            <>
+              <Spacer height={5} />
+              <Button
+                onPress={() =>
+                  navigation.navigate('NewsListByCategory', {
+                    section: item,
+                  })
+                }>
+                <ButtonText>{item.toUpperCase()}</ButtonText>
+              </Button>
+              <Spacer height={5} />
+            </>
+          );
+        }}
+      />
+    </Container>
   );
 };
 
