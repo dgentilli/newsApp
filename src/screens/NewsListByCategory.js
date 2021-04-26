2b13754f5c3af11878a677e10cfdecaab5c5beb8
Dave Gentilli <dgentilli@gmail.com>
2021-04-26 15:17:47 +0300

    Integrate API for NewsListByCategory.

--- src/screens/NewsListByCategory.js
+++ src/screens/NewsListByCategory.js
@@ -1,6 +1,7 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import styled from 'styled-components';
+const axios = require('axios');
 
 import {Context} from '../../App';
 import {
@@ -14,11 +15,32 @@ import NewsCardVertical from '../components/global/NewsCardVertical';
 import ApiLogo from '../components/global/ApiLogo';
 import {newsFeedMockData} from '../mockData/newsFeedMockData';
 
+import {NYT_API_KEY} from '@env';
+
 const NewsListByCategory = ({route}) => {
+  const [articleData, setArticleData] = useState([]);
   const context = useContext(Context);
   const {theme} = context;
   const {params} = route;
+  const {section} = params;
   //console.log('newsListByCategory context: ', context);
+  useEffect(() => {
+    let root = `https://api.nytimes.com/svc/topstories/v2`;
+
+    const fetchNews = async () => {
+      try {
+        let response = await axios.get(
+          `${root}/${section}.json?api-key=${NYT_API_KEY}`,
+        );
+        //console.log('newsScroll response', response.data.results);
+        setArticleData(response.data.results);
+      } catch (error) {
+        console.log({error});
+      }
+    };
+    fetchNews();
+  }, [section]);
+
   return (
     <Container theme={theme}>
       <Spacer height={20} />
@@ -27,8 +49,8 @@ const NewsListByCategory = ({route}) => {
       </PrimaryHeading>
       <Spacer height={20} />
       <FlatList
-        data={newsFeedMockData}
-        keyExtractor={item => item}
+        data={articleData}
+        keyExtractor={item => item.title}
         renderItem={({item}) => {
           return <NewsCardVertical data={item} />;
         }}
