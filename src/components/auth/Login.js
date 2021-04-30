b67c510f78a036e26b3d64489a22dc716f648367
Dave Gentilli <dgentilli@gmail.com>
2021-04-30 09:12:52 +0300

    Add UI and local state management for login with email and password.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import styled from 'styled-components';
 
 import {
   Container,
@@ -10,9 +11,24 @@ import {
   ButtonText,
   Spacer,
 } from '../global/Main';
+import theme from '../../theme';
+
+const FormInput = styled.TextInput`
+  width: 70%;
+  height: ${props => props.height}px;
+  border-width: 1px;
+  border-color: ${theme.color.LIGHT_GRAY};
+  border-radius: 20px;
+  margin: 0 auto;
+  padding: 12px;
+  color: ${props =>
+    props.theme === 'dark' ? theme.color.TEXT_LIGHT : theme.color.TEXT_DARK};
+`;
 
 const Login = ({theme, setUserInfo, toggleLoginSignup}) => {
   const [loginError, setLoginError] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleAnonymousLogin = () => {
     firebase
       .auth()
@@ -57,15 +73,34 @@ const Login = ({theme, setUserInfo, toggleLoginSignup}) => {
       <Spacer height={30} />
       <PrimaryHeading theme={theme}>Login</PrimaryHeading>
       <Spacer height={30} />
-      <SecondaryHeading theme={theme}>Login As Guest</SecondaryHeading>
+      <SecondaryHeading theme={theme}>Just Passing By?</SecondaryHeading>
+      <SecondaryHeading theme={theme}>Login As A Guest</SecondaryHeading>
       <Spacer height={10} />
-
       <Button onPress={() => handleAnonymousLogin()}>
         <ButtonText>Guest Login</ButtonText>
       </Button>
       <Spacer height={30} />
+      <SecondaryHeading>Already Have An Account?</SecondaryHeading>
+      <SecondaryHeading>Login With Your Email And Password</SecondaryHeading>
+      <Spacer height={10} />
+      <FormInput
+        height={50}
+        theme={theme}
+        value={email}
+        placeholder="Email"
+        onChangeText={text => setEmail(text)}
+      />
+      <Spacer height={5} />
+      <FormInput
+        height={50}
+        theme={theme}
+        value={password}
+        placeholder="Password"
+        onChangeText={text => setPassword(text)}
+      />
+      <Spacer height={30} />
       <Button onPress={toggleLoginSignup}>
-        <ButtonText>Need to sign up?</ButtonText>
+        <ButtonText>Need To Sign Up?</ButtonText>
       </Button>
     </Container>
   ) : (
